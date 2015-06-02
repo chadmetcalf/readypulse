ebcbb018db95458628e7ab1fa9fdc562aac4621e
Chad Metcalf <cmetcalf@nerdery.com>
2015-06-01 20:57:13 -0500

    Client #to_album and #to_images

--- lib/readypulse/client.rb
+++ lib/readypulse/client.rb
@@ -2,36 +2,56 @@ module Readypulse
   require 'json'
 
   class Client
-    def self.instance(album_id: nil)
+    def self.instance
       @@client ||= Client.new
     end
 
     attr_accessor :album_id
 
-    def self.instance
-      @@client ||= Client.new
+    def initialize
     end
 
-    def album_id=(album_id)
-      reset_response! if @album_id != album_id
-      @album_id = album_id
+    def to_album(album_id: @album_id)
+      validate_album_id(album_id)
+      parsed_album_response
     end
 
-    def to_album(album_id:)
-    end
-
-    def to_images
-      response_content
+    def to_images(album_id: @album_id)
+      validate_album_id(album_id)
+      parsed_response_content.map{|v| v.fetch(:media, {}).fetch(:images, nil)}
+      # parsed_response_content.first
     end
 
   private
+
+    def validate_album_id(album_id)
+      unless album_id.is_a?(Fixnum)
+        fail ArgumentError.new("Incorrect album_id value: #{album_id.inspect}")
+      end
+
+      reset_response! if album_id_changed?(album_id)
+    end
+
+    def album_id_changed?(album_id)
+      changed = (@album_id == album_id)
+      @album_id = album_id
+      return changed
+    end
 
     def reset_response!
       @response = nil
     end
 
-    def response_content
-      response['content']
+    def parsed_album_response
+      parsed_response.tap{|hash| hash.delete(:content)}
+    end
+
+    def parsed_response_content
+      parsed_response[:content]
+    end
+
+    def parsed_response
+      JSON.parse(response,:symbolize_names => true)
     end
 
     def response
@@ -40,8 +60,8 @@ module Readypulse
 
     def response!
       @response = Net::HTTP.get(uri)
-      fail TimeoutError.new if @response.empty? || @response.nil?
       # TODO: What is the right error message here?
+      fail TimeoutError.new if @response.empty? || @response.nil?
       @response
     end
 
